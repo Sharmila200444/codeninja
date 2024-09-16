ac47c58becab84a8df3c94e9253cb420778fb8d8
Sharmila200444 <sharmilaelumalais@gmail.com>
2024-09-16 14:28:42 +0530

    Rverse an array

---
+++ Problems/Reverse an array.java
@@ -0,0 +1,21 @@
+import java.util.* ;
+import java.io.*; 
+import java.util.ArrayList;
+
+public class Solution 
+{
+    public static void reverseArray(ArrayList<Integer> arr, int m)
+    {
+        int k=arr.size();
+        int j=1;
+        for(int i=m+1; i<(m+1+(k-(m+1))/2); i++){
+            int temp=arr.get(i);
+            arr.set(i,arr.get(k-j));
+            arr.set(k-j,temp);
+            j++;
+            
+        }
+        
+        // Write your code here.
+    }
+}
